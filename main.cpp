4609cd1d06d1bef14d5c39d3e5a42b4be2806971
Taihou <ansonfeng1999@gmail.com>
2024-06-17 16:04:52 +0800

    Random is now working

--- main.cpp
+++ main.cpp
@@ -6,33 +6,34 @@
 
 using namespace std;
 
-// void init() {
-//     ios::sync_with_stdio(false);
-//     cin.tie(nullptr);
-//     cout.tie(nullptr);
-// }
+void init() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
 
 int main() {
-    // init();
+    init();
+    string txt = "", n = "";
+    vector<string> pool = {"a", "b", "c"};
     while (1) {
-        string txt; cin >> txt;
+        cin >> txt;
         cout << txt << "\n";
         cout << "please enter options" << "\n";
-        string n;
-        vector<string> pool;
-        while (cin >> n) pool.emplace_back(n);
-        // int k = pool.size();
+        
+        // while (cin >> n) pool.emplace_back(n);
+        int k = pool.size();
         // if(!k) cout << "等下先。\n";
         // else {
-            for (auto e : pool) {
-                cout << e << " ";
-            }
+        // for (auto e : pool) {
+        //     cout << e << " ";
+        // }
         // }
         
         // else {
-        //     uniform_int_distribution<int> d(0, k - 1);
-        //     mt19937 en(time(nullptr));
-        //     cout << pool[d(en)] << "\n";
+            uniform_int_distribution<int> d(0, k - 1);
+            mt19937 en(time(nullptr));
+            cout << pool[d(en)] << "\n";
         // }
     }
     return 0;
