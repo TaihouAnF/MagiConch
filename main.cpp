f80ac981040b1c8867c93d537e0bd92ac235a7cc
Taihou <ansonfeng1999@gmail.com>
2024-06-24 14:12:58 +0800

    Keep the console alive

--- main.cpp
+++ main.cpp
@@ -16,17 +16,20 @@ int main() {
     init();
     string n = "";
     vector<string> pool;
-    cout << "please enter options" << "\n";
-    while (cin >> n) {
-        pool.emplace_back(n);
-        if (cin.get() == '\n') break;
+    while (1) {
+        cout << "please enter options" << "\n";
+        while (cin >> n) {
+            pool.emplace_back(n);
+            if (cin.get() == '\n') break;
+        }
+        int k = pool.size();
+        if(!k) cout << "等下先。\n";
+        else {
+            uniform_int_distribution<int> d(0, k - 1);
+            mt19937 en(time(nullptr));
+            cout << pool[d(en)] << "\n";
+        }
     }
-    int k = pool.size();
-    if(!k) cout << "等下先。\n";
-    else {
-        uniform_int_distribution<int> d(0, k - 1);
-        mt19937 en(time(nullptr));
-        cout << pool[d(en)] << "\n";
-    }
+    
     return 0;
 }
