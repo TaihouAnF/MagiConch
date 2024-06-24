722dab3304603a4ee028313b0bbad4ab431e7a63
Taihou <ansonfeng1999@gmail.com>
2024-06-24 14:09:33 +0800

    Implement the mt19937 engine

--- main.cpp
+++ main.cpp
@@ -14,27 +14,19 @@ void init() {
 
 int main() {
     init();
-    string txt = "", n = "";
-    vector<string> pool = {"a", "b", "c"};
-    while (1) {
-        cin >> txt;
-        cout << txt << "\n";
-        cout << "please enter options" << "\n";
-        
-        // while (cin >> n) pool.emplace_back(n);
-        int k = pool.size();
-        // if(!k) cout << "等下先。\n";
-        // else {
-        // for (auto e : pool) {
-        //     cout << e << " ";
-        // }
-        // }
-        
-        // else {
-            uniform_int_distribution<int> d(0, k - 1);
-            mt19937 en(time(nullptr));
-            cout << pool[d(en)] << "\n";
-        // }
+    string n = "";
+    vector<string> pool;
+    cout << "please enter options" << "\n";
+    while (cin >> n) {
+        pool.emplace_back(n);
+        if (cin.get() == '\n') break;
+    }
+    int k = pool.size();
+    if(!k) cout << "等下先。\n";
+    else {
+        uniform_int_distribution<int> d(0, k - 1);
+        mt19937 en(time(nullptr));
+        cout << pool[d(en)] << "\n";
     }
     return 0;
 }
